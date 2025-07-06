5698f712d55d1609a4ae75ed060135e338d2a6a0
NoahRR <noahray216@gmail.com>
2025-07-06 17:39:26 -0400

    on live server fixes from past
    Merged live server changes with new changes on github. Did so manually
    with vim on live server.

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-rc@04_mry_3-$@2sq$b9%-9jp6q2eyxf4bsw9&&esj++aw&r)p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['noahrayroberts.com']
 
 if 'CODESPACE_NAME' in os.environ:
     codespace_name = os.getenv("CODESPACE_NAME")
@@ -127,7 +127,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "hello_world" / "static",
 ]
 
-STATIC_URL = "static/"
+STATIC_URL = "/public_static/static/"
+STATIC_ROOT = "/home/nexusor1/v3noahrayroberts.com-django/public_static/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ passenger_wsgi.py
@@ -0,0 +1,2 @@
+import os
+from hello_world.wsgi import application
